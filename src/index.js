a867c86cb872fc755abcc6480e4d4a056dc2dbb7
salcedoma <salcedoma.github@gmail.com>
2025-03-28 15:31:57 -0300

    Update imports (add new import from expand-category-buttons.js);
    >Iinitialize expandDropdowns() function;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { createDialog, newTask, newCategory } from "./newtask-buttons";
 import { addNewTask } from "./newForm";
+import { expandDropwdowns } from "./expand-category-buttons";
 
 const body = document.querySelector("body");
+expandDropwdowns();
