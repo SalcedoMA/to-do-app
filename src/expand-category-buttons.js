5da5e1978e90c665bf07645cc8e2e458887bb082
salcedoma <salcedoma.github@gmail.com>
2025-03-28 19:36:36 -0300

    Place queryselector inside expandDropdowns() function.
    >This way, the DOM is checked everytime time the function is called and not just at the start of the program.

--- src/expand-category-buttons.js
+++ src/expand-category-buttons.js
@@ -1,6 +1,6 @@
-const dropdownButtons = document.querySelectorAll(".dropdown-button");
 
 function expandDropwdowns() {
+    const dropdownButtons = document.querySelectorAll(".dropdown-button");
     for (let button of dropdownButtons) {
         const wrapper = button.closest("div").querySelector(".dropdown-wrapper")
         button.addEventListener("click", () => {
